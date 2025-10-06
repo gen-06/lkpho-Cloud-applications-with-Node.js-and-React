5151d51be4f078da30c61be860e7bac87a9e5dfa
gen-06 <genalpha089@gmail.com>
2025-10-06 10:38:09 -0400

    new_change

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -4,12 +4,28 @@ const express = require('express');
 // Create an instance of an Express application
 const app = new express();
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+//define route to fetch the month name based on a given number
+app.get("/fetchMonth/:num", (req, res) => {
+    //parse the number from the request parameters
+    let num = parseInt(req.params.num);
+    //check if the number s a valid month number
+    if (num < 1 || num > 12) {
+        //send an err message if the number is not valid
+        res.send("Not a valid month number");
+    } else {
+        //send the corresponding month if the number is valid
+        res.send(months[num - 1]);
+    }
+});
+
 // Initialize an array to store login details
 let loginDetails = [];
 
 // Define the root route to send a welcome message
 app.get("/", (req, res) => {
-    res.send("Welcome to the express server");
+    res.send("Welcome to the express server Mr.king");
 });
 
 // Define a route to send login details as a JSON string
